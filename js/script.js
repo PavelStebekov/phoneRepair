5aa023e8990c3cc4feb0fc01702ee4e27f19c0eb
PavelStebekov <118444944+PavelStebekov@users.noreply.github.com>
2023-03-03 16:36:23 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -3,14 +3,28 @@ const swiper = new Swiper('.services__slider', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-    spaceBetween: 80,
+    spaceBetween: 10,
     slidesPerView: 3,
     centeredSlides: true,
     loopedSlides: 3,
     loop: true,
     speed: 700,
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: true,
-    },
+    // autoplay: {
+    //     delay: 2000,
+    //     disableOnInteraction: true,
+    // },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 30,
+      },
+      // // when window width is >= 480px
+      // 1340: {
+      //   spaceBetween: 50,
+      // },
+      // // // when window width is >= 640px
+      // 1560: {
+      //   spaceBetween: 30,
+      // }
+    }
 });
